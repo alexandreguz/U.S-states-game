5b99c29d8a72677da587927493527224ee2e7fad
alexandreguz <guzalexandre@gmail.com>
2025-02-03 18:32:48 +0200

    creating a csv file

--- main.py
+++ main.py
@@ -12,21 +12,33 @@ states = StatesClass()
 
 data = pandas.read_csv("50_states.csv")
 states_list = data.state.to_list()
+correct_states_list = []
 
-states.state_amount = 0
-while states.state_amount < 50:
-    answer_state = screen.textinput(f"{states.state_amount}/50 - Gues the State", "What is another state name?").title()
-    print(answer_state)
+
+while len(correct_states_list) < 50:
+    answer_state = screen.textinput(f"{len(correct_states_list)}/50 - Gues the State", "What is another state name?").title()
+    if answer_state == "Exit":
+        break
     if answer_state in states_list:
         state_name_in_file = data[data.state == answer_state]
-        print(state_name_in_file)
         states.state_name = state_name_in_file
         states.goto(state_name_in_file.x.item(), state_name_in_file.y.item())
         states.update_state_name(answer_state)
-        states.state_amount += 1
+        correct_states_list.append(answer_state)
 
 
-turtle.mainloop()
+# creating a csv file with those states that were forgotten:
+list_forgotten_states = []
+for state in states_list:
+    if state not in correct_states_list:
+        list_forgotten_states.append(state)
+states_lo_learn = {
+    "learn" : list_forgotten_states
+}
+data = pandas.DataFrame(states_lo_learn)
+data.to_csv("states-to-learn.csv")
+
+
 
 
 
